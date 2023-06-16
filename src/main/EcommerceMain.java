8035531955a5968ef78a3481ad95a123f829b701
NayLinn <naylinnkyaw22@gmail.com>
2023-06-16 14:17:58 +0630

    add gitignore file

--- src/main/EcommerceMain.java
+++ src/main/EcommerceMain.java
@@ -4,6 +4,7 @@ public class EcommerceMain {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Test");
 		System.out.println("Changes");
+		System.out.println("Just Modified");
 	}
 
 }
